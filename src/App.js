cacc1f3a857f9123831830e58f48101096f011ce
unknown <gm.c.burzo@gmail.com>
2022-09-19 09:22:39 -0400

    init3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import FrontPage from './Pages/frontPage/frontPage';
+import CreatorPage from './Pages/creatorPage/creatorPage';
+import {Routes, Route, Outlet, Link} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <FrontPage/>
+      <Routes>
+        <Route path = "frontPage" element = {<FrontPage/>}/>
+        <Route path = "creator" element = {<CreatorPage/>}/>
+        <Route path = "*" element = {<h3>No Match</h3>}/>
+      </Routes>
     </div>
   );
 }
---
+++ src/Pages/creatorPage/creatorPage.js
@@ -0,0 +1,11 @@
+import './creatorPage.css';
+
+function CreatorPage() {
+    return (
+        <div className="creatorPage">   
+            Hello creator Page
+        </div>
+    );
+}
+
+export default CreatorPage;
