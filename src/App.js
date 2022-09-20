03958701a146693ed04396d73cf26cb1905f9146
unknown <gm.c.burzo@gmail.com>
2022-09-20 15:47:11 -0400

    init4

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import './App.css';
 import FrontPage from './Pages/frontPage/frontPage';
 import CreatorPage from './Pages/creatorPage/creatorPage';
-import {Routes, Route, Outlet, Link} from 'react-router-dom';
+import {Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Routes>
-        <Route path = "frontPage" element = {<FrontPage/>}/>
-        <Route path = "creator" element = {<CreatorPage/>}/>
+        <Route path = "/" element = {<FrontPage />}/>
+        <Route path = "creator" element = {<CreatorPage />}/>
         <Route path = "*" element = {<h3>No Match</h3>}/>
       </Routes>
     </div>
-  );
+  )
 }
 
 export default App;
